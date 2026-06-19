07c7f32bf1e3ad6170994a6e86110657e8bd1b66
Marina Nikiforova <niki12880@gmail.com>
2026-06-20 00:07:47 +0300

    Geometry

---
+++ sandbox/src/main/java/geometry.java
@@ -0,0 +1,9 @@
+public class geometry {
+    public static void main(String[] args)
+    {
+        var side = 7.;
+        System.out.println("Площадь квадрата со стороной " + side + " = " + (side * side));
+
+        
+    }
+}
