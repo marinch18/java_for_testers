a3a90ae9187b23467edaef21a0a15cfbd8cc0182
Marina Nikiforova <niki12880@gmail.com>
2026-06-20 00:20:18 +0300

    Эксперименты с функциями

--- sandbox/src/main/java/geometry.java
+++ sandbox/src/main/java/geometry.java
@@ -1,9 +1,28 @@
 public class geometry {
     public static void main(String[] args)
     {
-        var side = 7.;
-        System.out.println("Площадь квадрата со стороной " + side + " = " + (side * side));
+        printSquareArea(7.0);
+        printSquareArea(5.0);
+        printSquareArea(3.0);
 
-        
+        printRectangleArea(3.0, 5.0);
+        printRectangleArea(7.0, 9.0);
+    }
+
+    private static void printRectangleArea(double a, double b) {
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + rectangleArea(a, b));
+    }
+
+    private static double rectangleArea(double a, double b) {
+        return a * b;
+    }
+
+
+    static void printSquareArea(double a) {
+        System.out.println("Площадь квадрата со стороной " + a + " = " + squeareArea(a));
+    }
+
+    private static double squeareArea(double a) {
+        return a * a;
     }
 }
