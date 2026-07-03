0f6c00cb923cf01d5c73ed6c2e5e0f31c16acf1f
Marina Nikiforova <niki12880@gmail.com>
2026-07-03 08:26:57 +0300

    Лекция 2.4. Сравнение объектов (метод equals)

--- sandbox/src/main/java/ru/stqa/geometry/fugures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/fugures/Rectangle.java
@@ -1,5 +1,7 @@
 package ru.stqa.geometry.fugures;
 
+import java.util.Objects;
+
 public record Rectangle
         (
                 double a,
@@ -19,5 +21,18 @@ public record Rectangle
 
     private static double rectangleArea(double a, double b) {
         return a * b;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return (Double.compare(rectangle.a, this.a) == 0 && Double.compare(rectangle.b, this.b) == 0)
+                || Double.compare(rectangle.a, this.b) == 0 && Double.compare(rectangle.b, this.a) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return 1; // Objects.hash(a, b) - с оптимизацией
     }
 }
--- sandbox/src/test/java/ru/stqa/geometry/fugures/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/fugures/SquareTests.java
@@ -30,4 +30,28 @@ public class SquareTests {
             //OK
         }
     }
+
+    @Test
+    void testNonEquality() {
+        var s1 = new Square(5.0);
+        var s2 = new Square(5.0);
+        Assertions.assertEquals(s1, s2);
+    }
+
+    @Test
+    void testNotEquality() {
+        var s1 = new Square(5.0);
+        var s2 = new Square(4.0);
+        Assertions.assertNotEquals(s1, s2);
+    }
+
+    @Test
+    void testFail() {
+        var s1 = new Square(5.0);
+        var s2 = new Square(5.0);
+        Assertions.assertTrue(s1.equals(s2)); // "==" можно сравнивать только числа и bool
+    }
+    // метод equals генерируется автоматически если есть record
+
+
 }
