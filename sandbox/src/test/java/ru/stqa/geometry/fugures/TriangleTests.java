9e25ef6eb47b44f50737ef10f1f9a4c5abdafb65
Marina Nikiforova <niki12880@gmail.com>
2026-07-03 08:44:42 +0300

    Задание №4

--- sandbox/src/test/java/ru/stqa/geometry/fugures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/fugures/TriangleTests.java
@@ -35,4 +35,25 @@ public class TriangleTests {
         }
     }
 
+    @Test
+    void testEqualityTriangle1() {
+        var t1 = new Triangle(5.0, 4.0, 3.0);
+        var t2 = new Triangle(5.0, 4.0, 3.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEqualityTriangle2() {
+        var t1 = new Triangle(3.0, 5.0, 4.0);
+        var t2 = new Triangle(5.0, 4.0, 3.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEqualityTriangle3() {
+        var t1 = new Triangle(3.0, 5.0, 4.0);
+        var t2 = new Triangle(4.0, 5.0, 3.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
 }
