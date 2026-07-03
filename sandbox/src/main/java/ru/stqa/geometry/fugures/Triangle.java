ef48bf6360dbf42a9ecc10e438724e44c7711851
Marina Nikiforova <niki12880@gmail.com>
2026-07-03 21:57:47 +0300

    исправления по заданию 4.

--- sandbox/src/main/java/ru/stqa/geometry/fugures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/fugures/Triangle.java
@@ -40,7 +40,9 @@ public record Triangle(double a, double b, double c) {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Triangle triangle = (Triangle) o;
-        return     (Double.compare(this.a, triangle.a) == 0 && Double.compare(this.b, triangle.b) == 0 && Double.compare(this.c, triangle.c) == 0)
+        return (Double.compare(this.a, triangle.a) == 0 && Double.compare(this.b, triangle.b) == 0 && Double.compare(this.c, triangle.c) == 0)
+                || (Double.compare(this.a, triangle.a) == 0 && Double.compare(this.b, triangle.c) == 0 && Double.compare(this.c, triangle.b) == 0)
+                || (Double.compare(this.a, triangle.b) == 0 && Double.compare(this.b, triangle.a) == 0 && Double.compare(this.c, triangle.c) == 0)
                 || (Double.compare(this.a, triangle.b) == 0 && Double.compare(this.b, triangle.c) == 0 && Double.compare(this.c, triangle.a) == 0)
                 || (Double.compare(this.a, triangle.c) == 0 && Double.compare(this.b, triangle.a) == 0 && Double.compare(this.c, triangle.b) == 0)
                 || (Double.compare(this.a, triangle.c) == 0 && Double.compare(this.b, triangle.b) == 0 && Double.compare(this.c, triangle.a) == 0);
--- sandbox/src/test/java/ru/stqa/geometry/fugures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/fugures/TriangleTests.java
@@ -56,4 +56,14 @@ public class TriangleTests {
         Assertions.assertEquals(t1, t2);
     }
 
+    @Test
+    void testEquality2(){
+        var a = 2;
+        var b = 3;
+        var c = 4;
+        var triangle = new Triangle(a, b, c);
+        var triangle1 = new Triangle(a, c, b);
+        Assertions.assertEquals(triangle, triangle1);
+    }
+
 }
