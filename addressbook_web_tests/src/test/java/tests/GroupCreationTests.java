8991a966517ca4ebaca6ef9a208f8f20d2de5b39
Marina Nikiforova <niki12880@gmail.com>
2026-07-07 00:27:22 +0300

    лекция 3.3 + исправления ошибок

---
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -0,0 +1,25 @@
+package tests;
+
+import model.GroupData;
+import org.junit.jupiter.api.Test;
+
+public class GroupCreationTests extends TestBase {
+
+    @Test
+    public void CanCreateGroup() {
+        app.openGroupsPage();
+        app.createGroup(new GroupData("group name", "group header", "group footer"));
+    }
+
+    @Test
+    public void CanCreateGroupWithEmptyName() {
+        app.openGroupsPage();
+        app.createGroup(new GroupData());
+    }
+
+    @Test
+    public void CanCreateGroupWithNameOnly() {
+        app.openGroupsPage();
+        app.createGroup(new GroupData());
+    }
+}
