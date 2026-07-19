63563b6e6f1809b9b1bd327390e7f4bce7d867b7
Marina Nikiforova <niki12880@gmail.com>
2026-07-19 17:48:19 +0300

    Лекция 4.5. Параметризованные тесты (1 часть)

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -3,16 +3,30 @@ package tests;
 import model.GroupData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
-    @Test
-    public void CanCreateGroup() {
-        int groupCount = app.groups().getCount();
-        app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
-        int newGroupCount = app.groups().getCount();
-        Assertions.assertEquals(groupCount + 1, newGroupCount);
+    public static List<String> groupNameProvider() {
+        var result = new ArrayList<String>(List.of("group header", "group footer"));
+        for (int i = 0; i < 5; i++) {
+            result.add(randomString(i * 10));
+        }
+        return result;
     }
+
+//    @ParameterizedTest
+//    @ValueSource(strings = {"group name", "group name'"})
+//    public void CanCreateGroup(String name) {
+//        int groupCount = app.groups().getCount();
+//        app.groups().createGroup(new GroupData(name, "group header", "group footer"));
+//        int newGroupCount = app.groups().getCount();
+//        Assertions.assertEquals(groupCount + 1, newGroupCount);
+//    }
 
     @Test
     public void CanCreateGroupWithEmptyName() {
@@ -24,15 +38,12 @@ public class GroupCreationTests extends TestBase {
         app.groups().createGroup(new GroupData().withName("Name"));
     }
 
-    @Test
-    public void CanCreateMultipleGroups() {
-        int n = 5;
+    @ParameterizedTest
+    @MethodSource("groupNameProvider")
+    public void CanCreateMultipleGroups(String name) {
         int groupCount = app.groups().getCount();
-
-        for (int i = 0; i < n; i++) {
-            app.groups().createGroup(new GroupData(randomString(i * 10), "group header", "group footer"));
-        }
+        app.groups().createGroup(new GroupData(name, "group header", "group footer"));
         int newGroupCount = app.groups().getCount();
-        Assertions.assertEquals(groupCount + n, newGroupCount);
+        Assertions.assertEquals(groupCount + 1, newGroupCount);
     }
 }
