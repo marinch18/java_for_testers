d41f011f1c61106ad3f3b22d243e0a2ad3928e9e
Marina Nikiforova <niki12880@gmail.com>
2026-08-23 14:42:08 +0300

    Лекция 5.8. Описание конфигурации тестового стенда

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -7,13 +7,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Properties;
+
 public class ApplicationManager {
     public WebDriver driver;
     private LoginHelper session;
     private GroupHelper groups;
     private ContactHelper contacts;
+    private Properties properties;
 
-    public void init(String browser) {
+    public void init(String browser, Properties properties) {
+        this.properties = properties;
         if (driver == null) {
             if("firefox".equals((browser))) {
                 driver = new FirefoxDriver();
@@ -23,9 +27,9 @@ public class ApplicationManager {
                 throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
             }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit)); // когда будет завершаться вызвать driver.quit()
-            driver.get("http://localhost/addressbook/");
+            driver.get(properties.getProperty("web.baseUrl"));
             driver.manage().window().setSize(new Dimension(967, 691));
-            session().login("admin", "secret");
+            session().login(properties.getProperty("web.username"), properties.getProperty("web.password"));
         }
     }
 
