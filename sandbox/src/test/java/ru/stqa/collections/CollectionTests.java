6bbb147518a62285880fc219d9e8f69a9a015a6f
Marina Nikiforova <niki12880@gmail.com>
2026-09-18 12:36:15 +0300

    Лекция 7.4. Множества

--- sandbox/src/test/java/ru/stqa/collections/CollectionTests.java
+++ sandbox/src/test/java/ru/stqa/collections/CollectionTests.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CollectionTests {
 
@@ -24,7 +26,7 @@ public class CollectionTests {
     @Test
     void listTests() {
         //var list = List.of("a", "b", "c"); // не модифицируемый
-        var list = new ArrayList<>(List.of("a", "b", "c")); // модифицируемый
+        var list = new ArrayList<>(List.of("a", "b", "c", "a")); // модифицируемый
 
 //        list.add("a"); // модифицируемый - добавление по 1 элементу
 //        list.add("b");
@@ -35,5 +37,15 @@ public class CollectionTests {
         list.set(0, "d");
         Assertions.assertEquals("d", list.get(0));
 
+    }
+
+    @Test
+    void setTests() {
+        var set = new HashSet<>(List.of("a", "b", "c", "a"));
+        Assertions.assertEquals(3, set.size());
+        var element = set.stream().findAny().get();
+
+        set.add("d");
+        Assertions.assertEquals(4, set.size());
     }
 }
