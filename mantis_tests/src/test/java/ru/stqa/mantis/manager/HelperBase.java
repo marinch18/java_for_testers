b4b7ee5943c6b9f606d553eb5f1d1e2ea620efbd
Marina Nikiforova <niki12880@gmail.com>
2026-09-24 17:23:36 +0300

    Задание №17: Подготовить новый проект для нового учебного приложения

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/HelperBase.java
@@ -0,0 +1,36 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.By;
+
+import java.nio.file.Paths;
+
+public class HelperBase {
+    protected final ApplicationManager manager;
+
+    public HelperBase(ApplicationManager manager) {
+        this.manager = manager;
+    }
+
+    protected void click(By locator) {
+        manager.driver().findElement(locator).click();
+    }
+
+    protected void type(By locator, String text) {
+        click(locator);
+        manager.driver().findElement(locator).clear();
+        manager.driver().findElement(locator).sendKeys(text);
+    }
+
+    protected void attach(By locator, String file) {
+        if (file != null && !file.isEmpty()) {
+            var path = Paths.get(file).toAbsolutePath();
+            System.out.println("PHOTO PATH: " + path);
+            System.out.println("PHOTO EXISTS: " + java.nio.file.Files.exists(path));
+            manager.driver().findElement(locator).sendKeys(Paths.get(file).toAbsolutePath().toString());
+        }
+    }
+
+    protected boolean isElementPresent (By locator) {
+        return manager.driver().findElements(locator).size() > 0;
+    }
+}
